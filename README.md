43ef70aefb33ad529dc1bd31df6c92e0f3aebf88
Joshymar Moncada <joshymarmw@gmail.com>
2021-07-01 13:53:14 -0600

    Docker file update

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ Instrucciones api Cowsay:
   &nbsp; npms install cowsay <br>
 3) Iniciar el api con el comando node app.js.
 4) Desde Postman realizar las peticiones Get y Post.
+<br>
+<br>
+<br>
+Instrucciones Docker:
+
+1)Creamos la imagen de docker ejecutando el siguiente comando en la terminal:<br>
+&nbsp; docker build . -t jmoncadacr/cowsay <br>
+2)Inicializamos la imagen  ejecutando el siguiente comando en la terminal:<br>
+&nbsp; docker run -p 49160:8080 -d  jmoncadacr/cowsay <br>
+3) Desde Postman realizar las peticiones Get y Post.
--- app.js
+++ app.js
@@ -3,25 +3,23 @@ const express = require("express");
 var cowsay = require("cowsay")
 const app = express();
 
-app.listen(3000, () => {
- console.log("El servidor está inicializado en el puerto 3000");
+app.listen(8080, () => {
+ console.log("El servidor inicializado en el puerto 8080, realice su petición Get o Post");
 });
 
 
 
 app.post('/', function (req, res) {
-    res.send('POST request to the homepage');
-    console.log(cowsay.say({
-        text:"petición Post",
+    res.send(cowsay.say({
+        text:"Petición Post",
         e: "OO",
         T:"P"
     }));
   });
 
   app.get('/', function (req, res) {
-    res.send('GET request to the homepage');
-    console.log(cowsay.say({
-        text:"petición Get",
+    res.send(cowsay.say({
+        text:"Petición Get",
         e: "OO",
         T:"P"
     }));
